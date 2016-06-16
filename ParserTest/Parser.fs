07f2cbad10ca425e5564fcd55948d170b9fbeb54
Fritsefreek <rvremco@hotmail.com>
2016-06-16 17:26:51 +0200

    Redid most of the parsing

--- ParserTest/Parser.fs
+++ ParserTest/Parser.fs
@@ -1,13 +1,15 @@
 ﻿module Parser 
 
 open System
-open Dictionary
 
 // Return a string with all the words of the programm, words are characters sepparted by a space
 let getWords (text: string) =
-    let rawWords = (text.Split (' ','\n')) |> Array.toList
+    let rawWords = (text.Split (' ','\n','\r')) |> Array.toList
     let words = rawWords |> List.filter(fun words -> words = "" |> not)
     words
+
+// Define the Dictionary
+let dictionary:Map<string,int> = [] |> Map.ofList
 
 // Parse code and execute it
 let rec remcode (words: List<string>) (dictionary: Map<string, int>) (next:int) (stack: Map<int, float32>) = 
@@ -15,12 +17,32 @@ let rec remcode (words: List<string>) (dictionary: Map<string, int>) (next:int) 
         | true -> stack //"The program has been executed"
         | false ->
             match dictionary |> Map.containsKey words.[next], words.[next] with
-                | true, "var" ->
-                    let key = dictionary.Count
-                    let dictionary' = dictionary.Add (words.[next+1], (key+1))
-                    remcode words dictionary' (next+2) stack
                 | true, word ->  
-                    let stack' = action word stack dictionary
+                    remcode words dictionary (next+1) stack
+                | false, "var" ->
+                    let dictionary' = dictionary.Add (words.[next+1], 0)
+                    remcode words dictionary' (next+2) stack
+                | false, "store" ->
+                    let value = stack.Item 0
+                    let stack' = stack.Remove 0
+                    let key = stack.Count
+                    let stack' = stack'.Add(key, value)
+                    let dictionary' = dictionary.Add (words.[next-1], key) 
+                    remcode words dictionary' (next+1) stack'
+                | false, "print" ->
+                    let printingWord = words.[next-1]
+                    match dictionary |> Map.containsKey printingWord with
+                    | true -> 
+                        let dictionaryKey = dictionary.Item printingWord
+                        let itemFromStack = stack.Item dictionaryKey
+                        printfn "%f" itemFromStack
+                    | false -> printfn "%f"  (stack |> Seq.last).Value
+                    remcode words dictionary (next+1) stack
+                | false, "+" -> 
+                    let key = (stack |> Seq.last).Key
+                    let stack' = stack.Add ((key+1), (stack.Item 0 + stack.Item 1))
+                    let stack' = stack'.Remove 0
+                    let stack' = stack'.Remove 1
                     remcode words dictionary (next+1) stack'
                 | false, word -> 
                     match System.Single.TryParse(word) with
--- ParserTest/Program.fs
+++ ParserTest/Program.fs
@@ -2,15 +2,11 @@
 
 open System
 open Parser
-open Dictionary
 open System.IO
 
 // Read program from file, just because
 let programInput = File.ReadAllText("SomeProgram.remcode");
-
-
 let program = getWords programInput
-let defaultStack:Map<int, float32> = [(0, 0.f); (0, 0.f)] |> Map.ofList
 
 let programOutput = remcode program dictionary 0 Map.empty
 
