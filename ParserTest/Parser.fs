c9d116839fd66b97dc55e191c2702df48fb9dcea
Fritsefreek <rvremco@hotmail.com>
2016-06-16 20:01:07 +0200

    Redid stack key assignment

--- ParserTest/Parser.fs
+++ ParserTest/Parser.fs
@@ -23,47 +23,52 @@ let rec remcode (words: List<string>) (dictionary: Map<string, int>) (next:int) 
                     let dictionary' = dictionary.Add (words.[next+1], 0)
                     remcode words dictionary' (next+2) stack
                 | false, "store" ->
-                    let value = stack.Item 0
                     let key = stack.Count
-                    let stack' = stack.Remove 0
+                    let value = stack.Item key
+                    let stack' = stack.Remove key
                     let stack' = stack'.Add(key, value)
                     let dictionary' = dictionary.Add (words.[next-1], key) 
                     remcode words dictionary' (next+1) stack'
                 | false, "print" ->
                     let printingWord = words.[next-1]
-                    match dictionary |> Map.containsKey printingWord with
+                    let stack' = match dictionary |> Map.containsKey printingWord with
                     | true -> 
                         let dictionaryKey = dictionary.Item printingWord
                         let itemFromStack = stack.Item dictionaryKey
                         printfn "%f" itemFromStack
-                    | false -> printfn "%f"  (stack |> Seq.last).Value
-                    remcode words dictionary (next+1) stack
+                        stack
+                    | false -> 
+                        let key =  (stack |> Seq.last).Key
+                        let stack' = stack.Remove key
+                        printfn "%f"  (stack |> Seq.last).Value
+                        stack'
+                    remcode words dictionary (next+1) stack'
                 | false, "+" -> 
                     let word1 = words.[next-1]
                     let word2 = words.[next-2]
-                    let key = (stack |> Seq.last).Key
+                    let key = match stack.Count with
+                        | 0 -> 0
+                        | _ -> (stack |> Seq.last).Key
                     let stack' = match System.Single.TryParse(word1), System.Single.TryParse(word2) with
                     | (false, value1), (false, value2) ->
                         let dictionaryKey1 = dictionary.Item word1
-                        let itemFromStack1 = stack.Item (dictionaryKey1-1)
+                        let itemFromStack1 = stack.Item dictionaryKey1
                         let dictionaryKey2 = dictionary.Item word2
-                        let itemFromStack2 = stack.Item (dictionaryKey2-1)
+                        let itemFromStack2 = stack.Item dictionaryKey2
                         let stack' = stack.Add ((key+1), (itemFromStack1 + itemFromStack2))
                         stack'
                     | (true, value1), (true, value2) ->
-                        let stack' = stack.Add ((key+2), (value1 + value2))
-                        let stack' = stack'.Remove key
-                        let stack' = stack'.Remove (key+1)
+                        let stack' = stack.Remove key
+                        let stack' = stack'.Remove (key-1)
+                        let stack' = stack'.Add ((key-1), (value1 + value2))
                         stack'
                     remcode words dictionary (next+1) stack'
                 | false, word -> 
                     match System.Single.TryParse(word) with
                     | (true, number) ->
-                        let position = 
-                            match stack.ContainsKey 0 with
-                            | true -> 1
-                            | false -> 0
-                        remcode words dictionary (next+1) (stack.Add (position, number))
+                        let key = stack.Count+1
+                        let stack' = stack.Add (key, number)
+                        remcode words dictionary (next+1) stack'
                     | _ ->
                         printfn "Unknown character found: %s" word
                         remcode words dictionary (next+1) stack
